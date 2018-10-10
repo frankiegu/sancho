9e215bb135a109459e60b18967422fde5223a3a7
davidnotes <gujinxin.dell@gmail.com>
2018-10-10 23:55:07 +0800

    move app.js and agent.js to satify eggjs convention

---
+++ server/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = app => {
+  // app.beforeStart(async () => {
+  //   // worker 进程被 fork 的话，会执行这段代码
+  // });
+
+  app.logger.info('app start');
+};
--- server/app/app.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = app => {
-  app.beforeStart(async () => {
-    // worker 进程被 fork 的话，会执行这段代码
-  });
-};
